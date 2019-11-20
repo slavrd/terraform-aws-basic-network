2e6211e624156c71b3210a89cd41b2f73910f30e
Slav Donchev <slav.donchev@gmail.com>
2019-11-20 13:02:13 +0200

    make `aws_nat_gateway` creation conditional

--- routing.tf
+++ routing.tf
@@ -1,3 +1,7 @@
+locals{
+  isNATGW = length(var.private_subnet_cidrs) == 0 ? false : true
+}
+
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
   tags   = var.common_tags
@@ -8,7 +12,7 @@ resource "aws_route_table" "public" {
   tags   = var.common_tags
 }
 
-resource "aws_route" "default" {
+resource "aws_route" "default_public" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
@@ -21,18 +25,21 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_eip" "nat_gw" {
+  count = local.isNATGW ? 1 : 0
   tags = var.common_tags
 }
 
 resource "aws_nat_gateway" "gw" {
-  allocation_id = aws_eip.nat_gw.id
+  count = local.isNATGW ? 1 : 0
+  allocation_id = aws_eip.nat_gw[0].id
   subnet_id     = aws_subnet.public[var.public_subnet_cidrs[0]].id
   depends_on    = [aws_internet_gateway.gw]
   tags          = var.common_tags
 }
 
 resource "aws_route" "default_nat_gw" {
+  count = local.isNATGW ? 1 : 0
   route_table_id         = aws_vpc.main.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.gw.id
+  nat_gateway_id         = aws_nat_gateway.gw[0].id
 }
